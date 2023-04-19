af149c5f9e05c71a1ec583ef7432f486f4864250
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-04-19 17:01:30 +0530

    multi page

--- js/login.js
+++ js/login.js
@@ -16,13 +16,49 @@ function checkLogin(event){
     let email = document.querySelector('#email');
     let password = document.querySelector('#password');
 
+    if(email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1 || email.value.indexOf("@") > email.value.lastIndexOf("."))
+     {
+        alert("Email is invalid")
+        return
+     }
+
     for(let t of user_data){
-        if(t.email == email.value && t.password == password.value){
-            alert("login successful")
+        if(t.email == email.value){
+            if(t.password == password.value){
+                alert("Login Successful")   
+            }
+            else{
+                alert("Password is incorrect")
+            }
            
         }
         else{
-            alert("login failed")
+            alert("User does not exist")
         }
+        generateToken(email.value)
+        localStorage.setItem('user_data', JSON.stringify(user_data))
+    }
+
+
+    function generateToken(email){
+        let collection = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789$-_.+!*'(),"
+        let token = ""
+
+        while(token.length<10){
+              token = token + collection[parseInt(Math.random()*collection.length)]
+              if(token.length == 10){
+                 let matchedUser =  user_data.filter(user => user.token == token)
+
+                 if(matchedUser.length > 0){
+                     token = ""
+                 }
+              }
+        }
+
+         let users =  user_data.filter(user => user.email == email)
+          users[0].token = token
+
+        
+
     }
 }
